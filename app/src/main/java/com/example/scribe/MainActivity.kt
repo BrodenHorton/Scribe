f881168f0483711921d51df57f1d38d0cee16882
SDRenegade <brody_horton@hotmail.com>
2026-01-07 20:24:28 -0700

    View is now snapped to the bottom of the speech blocks column when a update is made to any speech block or speech line.

--- app/src/main/java/com/example/scribe/MainActivity.kt
+++ app/src/main/java/com/example/scribe/MainActivity.kt
@@ -10,21 +10,27 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.IntrinsicSize
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.paddingFrom
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.mutableStateListOf
 import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.AlignmentLine
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -129,10 +135,18 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun speechBlocksLazyColumn(input: MutableList<SpeechBlock>) {
-    var speechBlocks = remember { input }
+    val speechBlocks = remember { input }
+    val listState = rememberLazyListState()
+
+    LaunchedEffect(speechBlocks.last().speechLines.last().text.value) {
+        if (speechBlocks.isNotEmpty())
+            listState.animateScrollToItem(index = speechBlocks.lastIndex + 1)
+    }
 
     LazyColumn(
+        state = listState,
         verticalArrangement = Arrangement.spacedBy(8.dp),
+        contentPadding = PaddingValues(bottom = 80.dp),
         modifier = Modifier
             .fillMaxSize()
             .background(Color.White)
